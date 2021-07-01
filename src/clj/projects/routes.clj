5de447d0851e86182e05def60e40846c8e899ebd
DennisK <dennisk@omicron.se>
2021-07-01 08:52:57 +0200

    Fix backend

--- src/clj/projects/routes.clj
+++ src/clj/projects/routes.clj
@@ -34,7 +34,7 @@
                          ;coerce-exceptions-middleware
                          swagger/swagger-feature]}})
    (ring/routes
-    (swagger-ui/create-swagger-ui-handler {:path "/doc"})
+    (swagger-ui/create-swagger-ui-handler {:path "/backend/doc"})
     (ring/redirect-trailing-slash-handler {:method :both})
     (ring/create-default-handler
      (:not-found (constantly {:status 404
